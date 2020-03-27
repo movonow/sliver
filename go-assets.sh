24975dfb2a7d5816a633b8ef9c4757fd65cd1724
movonow <39987032+movonow@users.noreply.github.com>
2020-03-27 16:34:30 +0800

    Update go-assets.sh

--- go-assets.sh
+++ go-assets.sh
@@ -123,9 +123,17 @@ zip -r protobuf.zip ./protobuf
 cp -vv protobuf.zip $REPO_DIR/assets/protobuf.zip
 
 wget -O master.tar.gz https://github.com/golang/sys/archive/master.tar.gz
-tar xfv master.tar.gz
-rm -rf master.tar.gz
-mv sys-master sys
+#tar xfv master.tar.gz
+#rm -rf master.tar.gz
+#mv sys-master sys
+#zip -r $REPO_DIR/assets/golang_x_sys.zip sys
+
+mkdir $WORK_DIR/sys                                                                                                                                                                       
+cd $WORK_DIR/sys                                                                                                                                                                          
+tar xfv ../master.tar.gz                                                                                                                                                                  
+rm -rf ../master.tar.gz                                                                                                                                                                   
+mv sys-master/* . && rmdir sys-master                                                                                                                                                     
+cd ..                                                                                                                                                                                     
 zip -r $REPO_DIR/assets/golang_x_sys.zip sys
 
 # end
